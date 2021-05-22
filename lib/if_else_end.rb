8a734d1ca5c09b5c19baebde7bb02cb04b073400
ambitious-caller-8426 <anhcdang@gmail.com>
2021-05-22 16:07:17 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,11 @@
 # Write your solution here
+
+# get current time
+current_time = Time.now
+
+# convert current time to int and determine if even or odd
+if current_time.to_i % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
